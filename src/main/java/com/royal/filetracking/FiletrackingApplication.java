5f129e4d376c0a40dfbe803d32c7fd1b44e41b84
savan <kansagrasavan108@gmail.com>
2023-03-04 11:39:40 +0530

    backend maven structure

---
+++ src/main/java/com/royal/filetracking/FiletrackingApplication.java
@@ -0,0 +1,13 @@
+package com.royal.filetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/royal/filetracking/FiletrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royal.filetracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiletrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
